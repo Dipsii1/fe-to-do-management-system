7f7b1e23ceee66a47bab8efd4ba7d769c6419398
Dipsii1 <nadifaryastia@gmail.com>
2025-07-05 12:20:22 +0700

    fix: fix in deploy error

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,30 @@
-import eslintPluginReact from 'eslint-plugin-react';
+import js from '@eslint/js'
+import eslintPluginReact from 'eslint-plugin-react'
 
 export default [
   {
     files: ['**/*.js', '**/*.jsx'],
+    languageOptions: {
+      ...js.configs.recommended.languageOptions,
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      ecmaFeatures: {
+        jsx: true,
+      },
+    },
     plugins: {
       react: eslintPluginReact,
     },
     rules: {
+      ...js.configs.recommended.rules,
       'react/jsx-uses-react': 'error',
       'react/jsx-uses-vars': 'error',
+      'react/react-in-jsx-scope': 'off',
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
     },
   },
-];
+]
